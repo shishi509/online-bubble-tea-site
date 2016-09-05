41df665b45c58213df0dcd6ceeb12feb33a109ca
shishi509 <shirui_zhang@outlook.com>
2016-09-06 01:05:07 +0800

    updated about us page with bootstrap

--- about_us.php
+++ about_us.php
@@ -3,6 +3,16 @@
 <head>
 <link rel="stylesheet" href="styleaboutus.css">
 <link rel="stylesheet" href="styles/style.css" media="all"/>
+<link href="styles/bootstrap.min.css" rel="stylesheet">
+<!-- Custom CSS -->
+<link href="styles/round-about.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
 	<!--Main container starts here-->
@@ -26,7 +36,56 @@
 		<!--Content wrapper starts-->
 		<div class="content_wrapper">
 		<div id="content_area">
-		About us
+		    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Introduction Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">About Us
+                    <small>It's Nice to Meet You!</small>
+                </h1>
+                <p>We are a team of editors working in an IT company Microhard who have co-founded the online bubble tea shop in 2016. <br>We are foodies who are never tired of discussing about, exploring and sharing good food. </p>
+            </div>
+        </div>
+
+        <!-- Team Members Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Our Team</h2>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="images/aboutusX.jpg" alt="">
+                <h3>Xinya Cai<br>
+                    <small>Recipe Developer</small>
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="images/aboutusL.jpg" alt="">
+                <h3>Lorraine Lai<br>
+                    <small>Process Optimizer</small>
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="images/aboutusS.jpg" alt="">
+                <h3>Stephanie Gu<br>
+                    <small>Bubble Tea Specialist</small>
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="images/aboutusZ.jpg" alt="">
+                <h3>Shirui Zhang<br>
+                    <small>Product Taster</small>
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+        </div>
+
+        <hr>
+
 		</div>
 		</div>
 	</div>  
