58441cdafb27f9b5fba65a45a1e418afe296424f
shishi509 <shirui_zhang@outlook.com>
2016-10-05 23:19:53 +0800

    css readnotes

--- readnotes.php
+++ readnotes.php
@@ -3,14 +3,20 @@
 	date_default_timezone_get('Asia/Singapore');
 	include 'dbconnectionnote.php';
 ?>
+<html>
+	<head>
+		<title>Read My Notes</title>
+<!--[if IE 9]><link rel="stylesheet" href="css/style-ie9.css"><![endif]-->
+<link rel="stylesheet" href="readnotes.css">
+	</head>
 <?php 
 function getComments($conn){
 	$sql = "SELECT * FROM work_notes";
 	$result = mysql_query($sql);
 	while ($row = mysql_fetch_assoc($result)){
 	echo "<div class='comment-box'><p>";
-		echo $row['title']."<br>";
-		echo $row['datetime']."<br>";	
+		echo $row['title']."<br>"."<br>";
+		echo $row['datetime']."<br>"."<br>";	
 		echo nl2br($row['note']); /*get line breaks*/
 
 }}
